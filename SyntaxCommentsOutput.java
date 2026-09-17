c3598b1ea66ca85ea3534b64a5c570b44df2b4a1
Dhanushiya-30 <KS.Dhanushiya@cognizant.com>
2026-09-17 11:44:26 +0530

    Add files via upload

---
+++ SyntaxCommentsOutput.java
@@ -0,0 +1,23 @@
+/*
+Hello World with comments
+This program prints name, roll number and place with single and multi-line comments
+*/
+class SyntaxCommentsOutput{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        //Single line comment :Used to describe about the code in a line)
+        //To get to know the name
+        System.out.println("Dhanu");
+        // To get the roll number
+        System.out.println("205");
+        //To get the place
+        System.out.println("Chennai");
+
+        /*
+        Multi-line comment 
+        It is used to describe about the code in multiple line 
+        not in single line 
+        */
+    }
+}
