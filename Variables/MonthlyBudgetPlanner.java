5a6dca95d3479d5421a3d26f7fcaed2092b9acf6
Dhanushiya-30 <KS.Dhanushiya@cognizant.com>
2026-09-17 15:15:59 +0530

    Added Monthly Budget Planner

---
+++ Variables/MonthlyBudgetPlanner.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class MonthlyBudgetPlanner{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the income: ");
+        double income=sc.nextDouble();
+        System.out.print("Enter the rent amount: ");
+        double rent=sc.nextDouble();
+        System.out.print("Enter the food expenses: ");
+        double food=sc.nextDouble();
+        System.out.print("Enter the transport amount: ");
+        double trans=sc.nextDouble();
+        System.out.print("Enter the shopping amount: ");
+        double shop=sc.nextDouble();
+        System.out.print("Enter the movie expenses: ");
+        double movies=sc.nextDouble();
+        System.out.print("Enter the mutual fund investment: ");
+        double funds=sc.nextDouble();
+        double total=rent+food+trans+shop+movies+funds;
+        double bal=income - total;
+
+        System.out.println();
+
+        System.out.println("======================================");
+        System.out.println("        MONTHLY BUDGET PLANNER        ");
+        System.out.println("======================================");
+        System.out.println();
+        System.out.println("Income            : "+income);
+        System.out.println();
+        System.out.println("BASIC NEEDS");
+        System.out.println("--------------------------------------");
+        System.out.println("Rent              : "+rent);
+        System.out.println("Food              : "+food);
+        System.out.println("Transport         : "+trans);
+        System.out.println();
+        System.out.println("EXTRA EXPENSES");
+        System.out.println("--------------------------------------");
+        System.out.println("Shopping          : "+shop);
+        System.out.println("Movies            : "+movies);
+        System.out.println();
+        System.out.println("INVESTMENTS");
+        System.out.println("--------------------------------------");
+        System.out.println("Mutual Funds      : "+funds);
+        System.out.println();
+        System.out.println("--------------------------------------");
+        System.out.println("Total Expenses    : "+total);
+        System.out.println("Balance Available : "+bal);
+        System.out.println("======================================");
+    }
+}
